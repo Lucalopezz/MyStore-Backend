432851527d3b33f83502bf1a5499ed954e4008f6
Lucas <lucaslops12@gmail.com>
2025-02-01 13:25:49 -0300

    implement logging system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from 'src/user/user.module';
 import { ProductModule } from 'src/product/product.module';
 import { CartModule } from 'src/cart/cart.module';
 import { OrderModule } from 'src/order/order.module';
+import { WinstonModule } from 'nest-winston';
+import { winstonConfig } from 'src/common/config/winston.config';
+import { UserLoggingMiddleware } from 'src/middlewares/userLogging.middleware';
 
 @Module({
-  imports: [UserModule, ProductModule, CartModule, OrderModule],
+  imports: [
+    UserModule,
+    ProductModule,
+    CartModule,
+    OrderModule,
+    WinstonModule.forRoot(winstonConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(UserLoggingMiddleware).forRoutes('*');
+  }
+}
---
+++ src/common/config/winston.config.ts
@@ -0,0 +1,30 @@
+import * as winston from 'winston';
+import * as path from 'path';
+import * as fs from 'fs';
+
+const logDir = path.join(process.cwd(), 'logs');
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+
+const logFormat = winston.format.combine(
+  winston.format.timestamp(),
+  winston.format.json(),
+);
+
+export const winstonConfig = {
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    }),
+    new winston.transports.File({
+      filename: path.join(logDir, 'combined.log'),
+      format: logFormat,
+    }),
+    new winston.transports.File({
+      filename: path.join(logDir, 'error.log'),
+      level: 'error',
+      format: logFormat,
+    }),
+  ],
+};
